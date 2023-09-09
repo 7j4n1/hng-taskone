efba23b37cb2bebeba82d73d268f7d75ed660191
7j4n1 <ahmaholy@gmail.com>
2023-09-09 08:40:13 +0100

    web fix

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,27 @@ use Illuminate\Support\Facades\Validator;
 |
 */
 
-Route::get('/', function(Request $request){
-    return view('welcome');
+Route::get('/api', function(Request $request){
+    $data = $request->only('slack_name', 'track');
+    $validator = Validator::make($data, [
+        'slack_name' =>'required|string',
+        'track' => 'required|string'
+    ]);
+
+    if($validator->fails()){
+        return response()->json(['error' => $validator->messages()], 200); 
+    }
+
+    $cur_date = new DateTime('now', new DateTimeZone('UTC')); 
+    $response = response()->json([
+        "slack_name" => $request->slack_name,
+        "current_day" => date('l'),
+        "utc_time"=> $cur_date->format('Y-m-d\TH:i:sp'),
+        "track" => "backend",
+        "github_file_url" => 'https://github.com/7j4n1/hng-taskone/blob/main/routes/api.php',
+        "github_repo_url" => "https://github.com/7j4n1/hng-taskone",
+        "status_code" => 200,
+    ], 200, [], JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+
+    return $response;
 });
